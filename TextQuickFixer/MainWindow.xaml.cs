0b727c50557901dd41a9b9ecf8c2d40d17c5601b
evesystem <547929833@qq.com>
2018-07-26 16:47:00 +0800

    eve

---
+++ TextQuickFixer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TextQuickFixer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StringFixer stringFixer = new StringFixer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_remove_n_r_Click(object sender, RoutedEventArgs e)
+        {
+            var output=stringFixer.remove_n_r(tb_input.Text);
+            tb_output.Text = output;
+        }
+
+        private void btn_remove_blanks_Click(object sender, RoutedEventArgs e)
+        {
+            var output = stringFixer.removeBlanks(tb_input.Text);
+            tb_output.Text = output;
+        }
+
+        private void btn_up_Click(object sender, RoutedEventArgs e)
+        {
+            tb_input.Text = tb_output.Text;
+        }
+
+        private void btn_sb_Click(object sender, RoutedEventArgs e)
+        {
+            tb_output.Text = stringFixer.sb_Append(tb_input.Text);
+        }
+    }
+}
---
+++ TextQuickFixer/StringFixer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextQuickFixer
+{
+    class StringFixer
+    {
+        public string remove_n_r(string  inputStr) 
+        {
+            var outputString = inputStr.Replace("\n", "");
+            outputString = outputString.Replace("\r", "");
+            return outputString;
+        }
+        /// <summary>
+        /// 移除多余空格
+        /// </summary>
+        /// <returns></returns>
+        public string removeBlanks(string str)
+        {
+            int len0 = str.Length;
+            int len1 = 0;
+            do
+            {
+                len0 = len1;
+                str = str.Replace("  ", " ");
+                len1 = str.Length;
+            } while (len1!=len0);
+            return str;
+
+        }
+
+        public string sb_Append(string str)
+        {
+            
+            var strArray = str.Split('\n');
+            var output = "";
+            foreach (var s in strArray)
+            {
+                var ss = s.Replace("\n","").Replace("\r", "");
+                output = output + "sb.Append(\" " + ss + " \");\n";
+            }
+            output = removeBlanks(output);
+            return output;
+        }
+    }
+}
